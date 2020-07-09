fe00536f5a8100a07dc50487178fd8f286395342
Peter Marklund <peter.marklund@schibsted.com>
2020-07-09 13:28:39 +0200

    Refactoring - extracted crud functions from app module into model

--- app.py
+++ app.py
@@ -1,54 +1,29 @@
 from flask import Flask, jsonify, make_response, request
-import db
-from datetime import datetime
 import models.urls
-from jsonschema import validate, ValidationError
 
 app = Flask(__name__)
 
-@app.errorhandler(ValidationError)
-def handle_validation_error(error):
-    response = jsonify({'message': error.message, 'schema': error.schema})
-    response.status_code = 400
-    return response
+# TODO: status 500 handler that returns JSON
 
-def not_found_response(message = 'Resource not found'):
-    return make_response(jsonify({'message': 'Resource not found'}), 404)
+def flask_response(result):
+    return make_response(jsonify(result.get('body', {})), result.get('status', 200))
 
 @app.route('/v1/urls', methods = ['GET'])
 def urls_list():
-    rows = db.query('select * from urls')
-    return jsonify(rows)
+    return flask_response(models.urls.list())
 
 @app.route('/v1/urls/<int:url_id>', methods = ['GET'])
 def urls_get(url_id):
-    row = db.query_one("select * from urls where id = %s", [url_id])
-    if not row:
-        return not_found_response()
-    return jsonify(row)
+    return flask_response(models.urls.get(url_id))
 
 @app.route('/v1/urls', methods = ['POST'])
 def urls_create():
-    validate(instance=request.json, schema=models.urls.json_schema)
-    doc = {**request.json, 'created_at': datetime.now()}
-    url_id = db.insert('urls', doc)
-    row = db.query_one("select * from urls where id = %s", [url_id])
-    return jsonify(row)
+    return flask_response(models.urls.create(request.json))
 
 @app.route('/v1/urls/<int:url_id>', methods = ['PUT'])
 def urls_update(url_id):
-    validate(instance=request.json, schema=models.urls.json_schema)
-    doc = request.json
-    db.update('urls', url_id, doc)
-    row = db.query_one("select * from urls where id = %s", [url_id])
-    if not row:
-        return not_found_response()
-    return jsonify(row)
+    return flask_response(models.urls.update(url_id, request.json))
 
 @app.route('/v1/urls/<int:url_id>', methods = ['DELETE'])
 def urls_delete(url_id):
-    row = db.query_one("select * from urls where id = %s", [url_id])
-    if not row:
-        return not_found_response()
-    db.execute('DELETE from urls where id = %s', [url_id])
-    return jsonify({})
+    return flask_response(models.urls.delete(url_id))
---
+++ json_schema.py
@@ -0,0 +1,12 @@
+from jsonschema import validate, ValidationError
+
+def validate_schema(instance, schema):
+  try:
+    validate(instance=instance, schema=schema)
+    return None
+  except ValidationError as schema_error:
+    return schema_error
+
+def validate_response(schema_error):
+  body = {'message': schema_error.message, 'schema': schema_error.schema}
+  return {'status': 400, 'body': body}
