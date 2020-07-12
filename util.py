9a5b692fd9dcecb5fd94833cc501f8171dfd4487
Peter Marklund <peter.marklund@schibsted.com>
2020-07-12 08:20:30 +0200

    is_dict_like function now also checks for presence of __contains__

--- util.py
+++ util.py
@@ -21,7 +21,7 @@ def get(value, keys, default_value = None):
     def valid_index(key):
         return re.match('^([1-9][0-9]*|[0-9])$', key) and int(key) >= 0
     def is_dict_like(v):
-        return hasattr(v, '__getitem__')
+        return hasattr(v, '__getitem__') and hasattr(v, '__contains__')
     for key in path:
         if isinstance(result, list) and valid_index(key) and int(key) < len(result):
             result = result[int(key)] if int(key) < len(result) else None
--- util_test.py
+++ util_test.py
@@ -1,13 +1,19 @@
 from util import get
 
 # See: https://stackoverflow.com/questions/4014621/a-python-class-that-acts-like-dict
-class DictLike(dict):
+class DictLike:
     def __init__(self, **dict):
       self.__dict__ = dict
     def __getitem__(self, key):
         return self.__dict__[key]
     def __contains__(self, item):
         return item in self.__dict__
+
+class NotDictLike:
+    def __init__(self, **dict):
+      self.__dict__ = dict
+    def __getitem__(self, key):
+        return self.__dict__[key]
 
 def test_get():
     assert get(None, ['foo']) == None
@@ -21,6 +27,7 @@ def test_get():
     assert get({'foo': {'bar': 'hello'}}, ['foo', 'bar']) == 'hello'
     assert get({'foo': {'bar': 'hello'}}, 'foo.bar') == 'hello'
     assert get(DictLike(**{'foo': {'bar': 'hello'}}), 'foo.bar') == 'hello'
+    assert get(NotDictLike(**{'foo': {'bar': 'hello'}}), 'foo.bar') == None
     assert get({'foo': [{'bar': 'hello'}]}, 'foo.0.bar') == 'hello'
     assert get({'foo': [{'bar': 'hello'}]}, 'foo.1') == None
     assert get({'foo': [{'bar': 'hello'}]}, 'foo.1.bar') == None
