e1df86b3bf0e7e40b6689fc4a73df321b7356262
Peter Marklund <peter.marklund@schibsted.com>
2020-07-18 19:53:44 +0200

    Added parameters to articles model and in readme

--- models/articles.py
+++ models/articles.py
@@ -1,18 +1,32 @@
 ARTICLES = [
   {
     'title': 'Why are so many corona patients struggling with lingering symptoms?'
+  },
+  {
+    'title': 'Lionel Messi labels Barcelona a "weak" team following shock defeat'
   }
 ]
 
 def list_articles(query, headers, **kwargs):
   print(f'query={query}')
   print(f'headers={headers}')
-  return {'body': {'data': ARTICLES}}
+  def is_match(article):
+    return 'q' not in query or query['q'].lower() in article['title'].lower()
+  data = [a for a in ARTICLES if is_match(a)]
+  return {'body': {'data': data}}
 
 routes = [
   {
     'method': 'GET',
     'path': '/v1/articles',
-    'handler': list_articles
+    'handler': list_articles,
+    'parameters': [
+      {
+        'in': 'query',
+        'name': 'q',
+        'description': 'Search query to filter by',
+        'schema': {'type': 'string'}
+      }
+    ]
   }
 ]
